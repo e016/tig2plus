24d7a1944ef36fbf9f6bbff35f57dbb5ad34bf41
e016 <dcdc6894@gmail.com>
2026-04-20 19:56:44 -0600

    rum n' bass update

--- game/game.js
+++ game/game.js
@@ -68737,7 +68737,7 @@ var version = "v1.10.10";
                         text:
                           "loading" === i
                             ? `${localize("LOADING")}...`
-                            : "COLLECTED!",
+                            : localize("COLLECTED!"),
                         color: Ye,
                         font: { size: 16 },
                         x: t / 2 - 90,
@@ -73328,7 +73328,7 @@ var version = "v1.10.10";
                           f
                             ? n({
                                 text:
-                                  "loading" === f ? "LOADING" : "COLLECTED!",
+                                  "loading" === f ? localize("LOADING") : localize("COLLECTED!"),
                                 color: Ye,
                                 font: { size: 16 },
                                 x: zu.hasIAP() ? 130 : 0,
@@ -73336,7 +73336,7 @@ var version = "v1.10.10";
                               })
                             : Fo({
                                 id: "CollectButton",
-                                text: "COLLECT",
+                                text: localize("COLLECT"),
                                 width: 120,
                                 height: 60,
                                 x: zu.hasIAP() ? 130 : 0,
